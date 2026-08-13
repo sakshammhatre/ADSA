6e7080d47191f9961b9764cd31bb0b2d0d1bd309
sakshammhatre <sakshamamhatre@gmail.com>
2026-08-13 11:57:27 +0530

    Add files via upload

---
+++ BSTtraversal.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class BST {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node root;
+
+    Node insert(Node root, int data) {
+        if (root == null)
+            return new Node(data);
+
+        if (data < root.data)
+            root.left = insert(root.left, data);
+        else
+            root.right = insert(root.right, data);
+
+        return root;
+    }
+
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BST tree = new BST();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            tree.root = tree.insert(tree.root, sc.nextInt());
+        }
+
+        System.out.print("Inorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+    }
+} 
